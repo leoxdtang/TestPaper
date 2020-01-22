26240c951af8e57d056ba25c3e4099dfe819a34d
leoxdtang <58928805+leoxdtang@users.noreply.github.com>
2020-01-22 15:46:03 +0800

    Initial Code

---
+++ Testpaper.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Test Paper程序：
+试卷：
+1.生成n道试题
+2.在console显示试题
+3.接受输入作为答案
+4.批改试卷
+5.输出分数和详细结果
+
+
+author: Leo Tang
+date : 2019.12.14
+"""
+
+if _name_=="main":
+    tp = Test_paper
+    tp.start_test()
+    
